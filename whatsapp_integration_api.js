4544d5d9a0b1990fa97e8e11ccfaae306e4900d1
unknown <jumbo.ti@jumboalimetos.com.br>
2024-09-26 18:05:04 -0300

    teste Api

--- whatsapp_integration_api.js
+++ whatsapp_integration_api.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const data = require("./data.json")
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) =>{
-    res.end('Rodando')
+app.use(bodyParser.json()) // 
+app.use(bodyParser.urlencoded({ extended: true })) 
+
+app.post('/confirm_cpf', function(req, res) {
+    res.json(data);
+});
+
+app.post("/confirm_cpf/:id", function(req, res){
+    console.log(req.body)
+    const { id } = req.params
+    const comfirmCpf = data.find(cpf => cpf.id == id)
+    if(req.body.cnpj == comfirmCpf.cnpj){
+        res.status(200).json();
+    }
+    else{
+        res.status(204).json();
+    }
 })
-
 app.listen(port, () => {
-    console.log("Servido rodando")
+    console.log("Server is running")
 })
